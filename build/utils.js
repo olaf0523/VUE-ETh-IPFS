a1eb0c87785b298bb996242dbd00b2ef93f607b1
Olaf_dev <kenzaumezaki@gmail.com>
2025-09-25 17:20:38 +0900

    fix: modify the bug

--- build/utils.js
+++ build/utils.js
@@ -16,7 +16,7 @@ exports.cssLoaders = function (options) {
   options = options || {}
 
   const cssLoader = {     
-    loader: 'css-loader',
+    loader: 'css-loader', 
     options: {
       sourceMap: options.sourceMap
     }
